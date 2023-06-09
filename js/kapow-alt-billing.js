ccab83127699f939a9520188a22b008a221ccfa0
Robert Galvin <54715512+robgalvinco@users.noreply.github.com>
2023-06-09 17:05:06 -0400

    Update kapow-alt-billing.js

--- js/kapow-alt-billing.js
+++ js/kapow-alt-billing.js
@@ -1,3 +1,6 @@
+/* Alternate Billing and Orders Page PowerUp by Rob Galvin for Thinkific site */
+/* https://www.superpowerups.com */
+/* expects kapow_alt_billing to be another url to redirect the billing and order page to otherwise it will redirect to the home page */
 if (window.location.href.includes("/account/orders") || window.location.href.includes("/account/billing")) {
   var kapow_alt_billing = typeof kapow_alt_billing !== 'undefined' && isValidURL(kapow_alt_billing) ? kapow_alt_billing : "/";
   window.location.href = kapow_alt_billing;
