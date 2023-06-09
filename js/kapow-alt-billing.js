eec9f27c01d48607a379bcae85673337ddbe03c4
Robert Galvin <54715512+robgalvinco@users.noreply.github.com>
2023-06-09 16:59:59 -0400

    Update kapow-alt-billing.js

--- js/kapow-alt-billing.js
+++ js/kapow-alt-billing.js
@@ -1,4 +1,5 @@
-if ((window.location.href.includes("/account/orders") || window.location.href.includes("/account/billing")) && typeof kapow_alt_billing !== 'undefined' && isValidURL(kapow_alt_billing)) {
+if (window.location.href.includes("/account/orders") || window.location.href.includes("/account/billing")) {
+  var kapow_alt_billing = typeof kapow_alt_billing !== 'undefined' && isValidURL(kapow_alt_billing) ? kapow_alt_billing : "/";
   window.location.href = kapow_alt_billing;
 }
 
