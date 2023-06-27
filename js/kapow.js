3cb0de1727aa79565a98fb6ed1641ebf6987562f
Robert Galvin <54715512+robgalvinco@users.noreply.github.com>
2023-06-27 15:27:53 -0400

    Update kapow.js

--- js/kapow.js
+++ js/kapow.js
@@ -1,6 +1,24 @@
-fetch('https://www.robgalvin.co/kapow/?l='+window.location.href)
-  .then(response => response.text())
-  .then(html => {
+const endpoint = "https://parseapi.back4app.com/functions/pagesbeacon";
+const headers = {
+  "X-Parse-Application-Id": "G3jWuOFhgW831cYxRbxwKlco8iklwdeKjRBI3GZx",
+  "X-Parse-REST-API-Key": "3dsu1YIrPHuRqZGhxy2JebN2Ii9kmWjhYTfuSTq7",
+  "Content-Type": "application/json"
+};
+const body = {
+  url: window.location.href
+};
+
+fetch(endpoint, {
+  method: "POST",
+  headers: headers,
+  body: JSON.stringify(body)
+})
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
+    //console.log(data);
   })
   .catch(error => {
+    // Handle any errors
+    //console.error(error);
   });
