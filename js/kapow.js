81549b71206bc40df59fb66c6f3629e4f951fc91
Robert Galvin <54715512+robgalvinco@users.noreply.github.com>
2023-07-04 05:24:47 -0400

    Update kapow.js - wrapped in function and removed sitebuilder

--- js/kapow.js
+++ js/kapow.js
@@ -1,27 +1,35 @@
-const endpoint = "https://parseapi.back4app.com/functions/pagesbeacon";
-const headers = {
-  "X-Parse-Application-Id": "G3jWuOFhgW831cYxRbxwKlco8iklwdeKjRBI3GZx",
-  "X-Parse-REST-API-Key": "3dsu1YIrPHuRqZGhxy2JebN2Ii9kmWjhYTfuSTq7",
-  "Content-Type": "application/json"
-};
-const currentUrl = window.location.href;
-const urlWithoutQueryParams = currentUrl.split('?')[0].split('#')[0];
+(function() {
+  const endpoint = "https://parseapi.back4app.com/functions/pagesbeacon";
+  const headers = {
+    "X-Parse-Application-Id": "G3jWuOFhgW831cYxRbxwKlco8iklwdeKjRBI3GZx",
+    "X-Parse-REST-API-Key": "3dsu1YIrPHuRqZGhxy2JebN2Ii9kmWjhYTfuSTq7",
+    "Content-Type": "application/json"
+  };
+  const currentUrl = window.location.href;
+  const urlWithoutQueryParams = currentUrl.split('?')[0].split('#')[0];
 
-const body = {
-  url: urlWithoutQueryParams
-};
+  // Check if the currentUrl contains "/manage/site_builder"
+  if (currentUrl.includes("/manage/site_builder")) {
+    // Skip the fetch POST request
+    return;
+  }
 
-fetch(endpoint, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(body)
-})
-  .then(response => response.json())
-  .then(data => {
-    // Handle the response data
-    //console.log(data);
+  const body = {
+    url: urlWithoutQueryParams
+  };
+
+  fetch(endpoint, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body)
   })
-  .catch(error => {
-    // Handle any errors
-    //console.error(error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data
+      // console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors
+      // console.error(error);
+    });
+})();
