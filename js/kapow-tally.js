16775b306a7416192d5b9ea997dc9afaee33e47b
sixgz <47870918+sixgz@users.noreply.github.com>
2024-08-09 17:17:46 -0400

    Update kapow-tally.js
    Change hid course footer to button (otherwise videos get smaller)

--- js/kapow-tally.js
+++ js/kapow-tally.js
@@ -4,7 +4,7 @@ window.addEventListener('message', (e) => {
         const payload = JSON.parse(e.data).payload;
         // ...
         console.log("Tally Form loaded");
-        $('#course-player-footer').hide();
+        $('#course-player-footer button').hide();
     }
 });   
 window.addEventListener('message', (e) => {
@@ -12,13 +12,13 @@ window.addEventListener('message', (e) => {
         const payload = JSON.parse(e.data).payload;
         // ...
         console.log("Tally form submitted");
-        $('#course-player-footer').show();    		
+        $('#course-player-footer button').show();    		
     }
 });    
 $(document).ready(function(){
     if(typeof(CoursePlayerV2) !== 'undefined') {
         CoursePlayerV2.on('hooks:contentDidChange', function(data) {
-            $('#course-player-footer').show();
+            $('#course-player-footer button').show();
         });
     }
 });
